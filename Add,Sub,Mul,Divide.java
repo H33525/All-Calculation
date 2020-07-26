e6ec4ca21707067ad3561d4375e158cf85956033
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-07-26 08:31:49 -0700

    Add files via upload

---
+++ Add,Sub,Mul,Divide.java
@@ -0,0 +1,40 @@
+import java.io.*;
+class All
+{
+public static void main(String args[])throws IO Exception
+{
+Buffered Reader br=new Buffered Reader(new Input Stream Reader(System.in);
+int a,b,c;
+System.out.print("Enter any Number);
+a=Integer.parseInt(br.readLine());
+b=Integer.parseInt(br.readLine());
+c=Integer.parseInt(br.readLine());
+Switch(c)
+{
+case 1:
+{
+c=a+b;
+System.out.print("Addtion is= "+c);
+break;
+}
+case 2:
+{
+c=a-b;
+System.out.print("Substraction is= "+c);
+break;
+}
+case 3:
+{
+c=a*b;
+System.out.print("Multiplication is= "+c);
+break;
+}
+case :
+{
+c=a/b;
+System.out.print("Divide is "+c);
+break;
+}
+}
+}
+}
